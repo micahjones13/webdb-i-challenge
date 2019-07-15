59e21bdcc61b1204c3ca25f08195eaf61395f80f
micahjones13 <micahjones13@gmail.com>
2019-07-15 14:14:15 -0600

    finished MVP and SQL stretch questions

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
+// const express = require('express');
+
+// const AccountRouter = require('./accounts/accounts-router.js');
+
+// // const db = require('./data/dbConfig.js');
+
+// const server = express();
+
+// server.use(express.json());
+// server.use('/api/accounts', AccountRouter);
+
+// server.get('/', (req, res) => {
+//     res.send('<h1>Web DB Challenge 1</h1>');
+// })
+// module.exports = server;
+
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const AccountRouter = require('./accounts/accounts-router.js');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', AccountRouter);
+
+server.get('/', (req, res) => {
+  res.send('<h3>DB Helpers with knex</h3>');
+});
 
 module.exports = server;
